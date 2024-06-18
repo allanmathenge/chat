c0172f287f0ad0df524b7d04107b11993035f0f9
unknown <allanmathenge2.com>
2024-06-18 12:34:22 +0300

    end of ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-// import Login from "./pages/login/Login";
+//import Login from "./pages/login/Login";
 // import SignUp from "./pages/signup/SignUp";
 import Home from "./pages/home/Home";
 
@@ -7,8 +7,9 @@ function App() {
 
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Home />
       {/* <SignUp /> */}
+      <Home />
+      {/* <Login /> */}
     </div>
   )
 }
