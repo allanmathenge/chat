e850f7ad92e242e85418255798154a5f8b377c04
unknown <allanmathenge2.com>
2024-06-20 02:30:12 +0300

    state management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css';
-//import Login from "./pages/login/Login";
-// import SignUp from "./pages/signup/SignUp";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
 import Home from "./pages/home/Home";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
 
+  const { authUser } = useAuthContext()
+
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      {/* <SignUp /> */}
-      <Home />
-      {/* <Login /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={< Login />} />
+        <Route path="/signup" element={authUser ? <Navigate to="/" /> : <SignUp /> } />
+      </Routes >
+      <Toaster />
     </div>
   )
 }
